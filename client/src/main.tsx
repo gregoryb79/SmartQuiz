e6c7abfa2d173149d8192bf9c9cf18db936ae00b
gregoryb79 <gregoryb79@gmail.com>
2025-05-22 12:16:12 +0300

    root for scss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 
-// import "./index.scss";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
